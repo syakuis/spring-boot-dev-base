42f037ef59effac32671d1b60e5127e479c7e402
최석균 <syaku@naver.com>
2019-01-12 20:09:50 +0900

    :)

---
+++ src/main/java/org/syaku/Application.java
@@ -0,0 +1,15 @@
+package org.syaku;
+
+/**
+ * @author Seok Kyun. Choi. 최석균 (Syaku)
+ * @since 2019-01-12
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
